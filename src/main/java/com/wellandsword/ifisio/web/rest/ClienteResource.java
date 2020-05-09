8b4041375786614c5909b223cc825461e8bf4368
x231458 <apozogar@everis.com>
2020-05-09 14:49:43 +0200

    cambios pantalla

--- src/main/java/com/wellandsword/ifisio/web/rest/ClienteResource.java
+++ src/main/java/com/wellandsword/ifisio/web/rest/ClienteResource.java
@@ -32,96 +32,109 @@ import java.util.Optional;
 @Transactional
 public class ClienteResource {
 
-    private final Logger log = LoggerFactory.getLogger(ClienteResource.class);
+	private final Logger log = LoggerFactory.getLogger(ClienteResource.class);
 
-    private static final String ENTITY_NAME = "cliente";
+	private static final String ENTITY_NAME = "cliente";
 
-    @Value("${jhipster.clientApp.name}")
-    private String applicationName;
+	@Value("${jhipster.clientApp.name}")
+	private String applicationName;
 
-    private final ClienteRepository clienteRepository;
+	private final ClienteRepository clienteRepository;
 
-    public ClienteResource(ClienteRepository clienteRepository) {
-        this.clienteRepository = clienteRepository;
-    }
+	public ClienteResource(ClienteRepository clienteRepository) {
+		this.clienteRepository = clienteRepository;
+	}
 
-    /**
-     * {@code POST  /clientes} : Create a new cliente.
-     *
-     * @param cliente the cliente to create.
-     * @return the {@link ResponseEntity} with status {@code 201 (Created)} and with body the new cliente, or with status {@code 400 (Bad Request)} if the cliente has already an ID.
-     * @throws URISyntaxException if the Location URI syntax is incorrect.
-     */
-    @PostMapping("/clientes")
-    public ResponseEntity<Cliente> createCliente(@RequestBody Cliente cliente) throws URISyntaxException {
-        log.debug("REST request to save Cliente : {}", cliente);
-        if (cliente.getId() != null) {
-            throw new BadRequestAlertException("A new cliente cannot already have an ID", ENTITY_NAME, "idexists");
-        }
-        Cliente result = clienteRepository.save(cliente);
-        return ResponseEntity.created(new URI("/api/clientes/" + result.getId()))
-            .headers(HeaderUtil.createEntityCreationAlert(applicationName, true, ENTITY_NAME, result.getId().toString()))
-            .body(result);
-    }
+	/**
+	 * {@code POST  /clientes} : Create a new cliente.
+	 *
+	 * @param cliente the cliente to create.
+	 * @return the {@link ResponseEntity} with status {@code 201 (Created)} and with
+	 *         body the new cliente, or with status {@code 400 (Bad Request)} if the
+	 *         cliente has already an ID.
+	 * @throws URISyntaxException if the Location URI syntax is incorrect.
+	 */
+	@PostMapping("/clientes")
+	public ResponseEntity<Cliente> createCliente(@RequestBody Cliente cliente) throws URISyntaxException {
+		log.debug("REST request to save Cliente : {}", cliente);
+		if (cliente.getId() != null) {
+			throw new BadRequestAlertException("A new cliente cannot already have an ID", ENTITY_NAME, "idexists");
+		}
+		Cliente result = clienteRepository.save(cliente);
+		return ResponseEntity
+				.created(new URI("/api/clientes/" + result.getId())).headers(HeaderUtil
+						.createEntityCreationAlert(applicationName, true, ENTITY_NAME, result.getId().toString()))
+				.body(result);
+	}
 
-    /**
-     * {@code PUT  /clientes} : Updates an existing cliente.
-     *
-     * @param cliente the cliente to update.
-     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and with body the updated cliente,
-     * or with status {@code 400 (Bad Request)} if the cliente is not valid,
-     * or with status {@code 500 (Internal Server Error)} if the cliente couldn't be updated.
-     * @throws URISyntaxException if the Location URI syntax is incorrect.
-     */
-    @PutMapping("/clientes")
-    public ResponseEntity<Cliente> updateCliente(@RequestBody Cliente cliente) throws URISyntaxException {
-        log.debug("REST request to update Cliente : {}", cliente);
-        if (cliente.getId() == null) {
-            throw new BadRequestAlertException("Invalid id", ENTITY_NAME, "idnull");
-        }
-        Cliente result = clienteRepository.save(cliente);
-        return ResponseEntity.ok()
-            .headers(HeaderUtil.createEntityUpdateAlert(applicationName, true, ENTITY_NAME, cliente.getId().toString()))
-            .body(result);
-    }
+	/**
+	 * {@code PUT  /clientes} : Updates an existing cliente.
+	 *
+	 * @param cliente the cliente to update.
+	 * @return the {@link ResponseEntity} with status {@code 200 (OK)} and with body
+	 *         the updated cliente, or with status {@code 400 (Bad Request)} if the
+	 *         cliente is not valid, or with status
+	 *         {@code 500 (Internal Server Error)} if the cliente couldn't be
+	 *         updated.
+	 * @throws URISyntaxException if the Location URI syntax is incorrect.
+	 */
+	@PutMapping("/clientes")
+	public ResponseEntity<Cliente> updateCliente(@RequestBody Cliente cliente) throws URISyntaxException {
+		log.debug("REST request to update Cliente : {}", cliente);
+		if (cliente.getId() == null) {
+			throw new BadRequestAlertException("Invalid id", ENTITY_NAME, "idnull");
+		}
+		Cliente result = clienteRepository.save(cliente);
+		return ResponseEntity.ok().headers(
+				HeaderUtil.createEntityUpdateAlert(applicationName, true, ENTITY_NAME, cliente.getId().toString()))
+				.body(result);
+	}
 
-    /**
-     * {@code GET  /clientes} : get all the clientes.
-     *
-     * @param pageable the pagination information.
-     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and the list of clientes in body.
-     */
-    @GetMapping("/clientes")
-    public ResponseEntity<List<Cliente>> getAllClientes(Pageable pageable) {
-        log.debug("REST request to get a page of Clientes");
-        Page<Cliente> page = clienteRepository.findAll(pageable);
-        HttpHeaders headers = PaginationUtil.generatePaginationHttpHeaders(ServletUriComponentsBuilder.fromCurrentRequest(), page);
-        return ResponseEntity.ok().headers(headers).body(page.getContent());
-    }
+	/**
+	 * {@code GET  /clientes} : get all the clientes.
+	 *
+	 * @param pageable the pagination information.
+	 * @return the {@link ResponseEntity} with status {@code 200 (OK)} and the list
+	 *         of clientes in body.
+	 */
+	@GetMapping("/clientes")
+	public ResponseEntity<List<Cliente>> getAllClientes(Pageable pageable) {
+		log.debug("REST request to get a page of Clientes");
+		Page<Cliente> page = clienteRepository.findAll(pageable);
+		for (Cliente cliente : page) {
+			cliente.getNumDocs().forEach(p -> p.getCompanya());
+		}
+		HttpHeaders headers = PaginationUtil
+				.generatePaginationHttpHeaders(ServletUriComponentsBuilder.fromCurrentRequest(), page);
+		return ResponseEntity.ok().headers(headers).body(page.getContent());
+	}
 
-    /**
-     * {@code GET  /clientes/:id} : get the "id" cliente.
-     *
-     * @param id the id of the cliente to retrieve.
-     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and with body the cliente, or with status {@code 404 (Not Found)}.
-     */
-    @GetMapping("/clientes/{id}")
-    public ResponseEntity<Cliente> getCliente(@PathVariable Long id) {
-        log.debug("REST request to get Cliente : {}", id);
-        Optional<Cliente> cliente = clienteRepository.findById(id);
-        return ResponseUtil.wrapOrNotFound(cliente);
-    }
+	/**
+	 * {@code GET  /clientes/:id} : get the "id" cliente.
+	 *
+	 * @param id the id of the cliente to retrieve.
+	 * @return the {@link ResponseEntity} with status {@code 200 (OK)} and with body
+	 *         the cliente, or with status {@code 404 (Not Found)}.
+	 */
+	@GetMapping("/clientes/{id}")
+	public ResponseEntity<Cliente> getCliente(@PathVariable Long id) {
+		log.debug("REST request to get Cliente : {}", id);
+		Optional<Cliente> cliente = clienteRepository.findById(id);
+		return ResponseUtil.wrapOrNotFound(cliente);
+	}
 
-    /**
-     * {@code DELETE  /clientes/:id} : delete the "id" cliente.
-     *
-     * @param id the id of the cliente to delete.
-     * @return the {@link ResponseEntity} with status {@code 204 (NO_CONTENT)}.
-     */
-    @DeleteMapping("/clientes/{id}")
-    public ResponseEntity<Void> deleteCliente(@PathVariable Long id) {
-        log.debug("REST request to delete Cliente : {}", id);
-        clienteRepository.deleteById(id);
-        return ResponseEntity.noContent().headers(HeaderUtil.createEntityDeletionAlert(applicationName, true, ENTITY_NAME, id.toString())).build();
-    }
+	/**
+	 * {@code DELETE  /clientes/:id} : delete the "id" cliente.
+	 *
+	 * @param id the id of the cliente to delete.
+	 * @return the {@link ResponseEntity} with status {@code 204 (NO_CONTENT)}.
+	 */
+	@DeleteMapping("/clientes/{id}")
+	public ResponseEntity<Void> deleteCliente(@PathVariable Long id) {
+		log.debug("REST request to delete Cliente : {}", id);
+		clienteRepository.deleteById(id);
+		return ResponseEntity.noContent()
+				.headers(HeaderUtil.createEntityDeletionAlert(applicationName, true, ENTITY_NAME, id.toString()))
+				.build();
+	}
 }
---
+++ src/main/webapp/app/tabla-cliente/tabla-cliente.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { IFisioSharedModule } from 'app/shared/shared.module';
+import { TablaClienteComponent } from './tabla-cliente.component';
+import { tablaClienteRoute } from './tabla-cliente.route';
+
+@NgModule({
+  imports: [IFisioSharedModule, RouterModule.forChild(tablaClienteRoute)],
+  declarations: [TablaClienteComponent]
+})
+export class IFisioTablaClienteModule {}
