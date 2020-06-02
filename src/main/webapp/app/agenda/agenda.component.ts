ed17b1950818379c7c56ceacb37c5334422516fe
Alberto Pozo <apozogar@everis.com>
2020-06-02 09:26:09 +0200

    cambios

--- src/main/webapp/app/agenda/agenda.component.ts
+++ src/main/webapp/app/agenda/agenda.component.ts
@@ -1,17 +1,20 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import interactionPlugin from '@fullcalendar/interaction';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { CitaDeleteDialogComponent } from 'app/entities/cita/cita-delete-dialog.component';
 import { CitaService } from 'app/entities/cita/cita.service';
 import { TratamientoClienteService } from 'app/entities/tratamiento-cliente/tratamiento-cliente.service';
 import { ICita } from 'app/shared/model/cita.model';
 import { ICliente } from 'app/shared/model/cliente.model';
 import { ITratamientoCliente } from 'app/shared/model/tratamiento-cliente.model';
 import * as moment from 'moment';
+import { JhiEventManager } from 'ng-jhipster';
 import { SelectItem } from 'primeng/api/selectitem';
-import { CitaDeleteDialogComponent } from 'app/entities/cita/cita-delete-dialog.component';
+import { Subscription } from 'rxjs';
+import { FullCalendar } from 'primeng/fullcalendar/public_api';
 
 @Component({
   selector: 'jhi-agenda',
@@ -20,6 +23,9 @@ import { CitaDeleteDialogComponent } from 'app/entities/cita/cita-delete-dialog.
 export class AgendaComponent implements OnInit, OnDestroy {
   events: any[] = [];
   citas: ICita[] = [];
+  citaSel: ICita = {};
+
+  eventSubscriber?: Subscription;
 
   options: any;
   cliente: ICliente;
@@ -36,10 +42,13 @@ export class AgendaComponent implements OnInit, OnDestroy {
     }
   ];
 
+  @ViewChild('fc') fc: FullCalendar | undefined;
+
   constructor(
     private citaService: CitaService,
     private tratamientoClienteService: TratamientoClienteService,
     protected modalService: NgbModal,
+    protected eventManager: JhiEventManager,
     protected activatedRoute: ActivatedRoute
   ) {
     this.cliente = {};
@@ -55,6 +64,8 @@ export class AgendaComponent implements OnInit, OnDestroy {
         this.citaService.query().subscribe((events: any) => this.loadCita(events.body));
       }
     });
+
+    this.registerChangeInCitaDelete();
 
     this.options = {
       plugins: [dayGridPlugin, timeGridPlugin, interactionPlugin],
@@ -96,7 +107,7 @@ export class AgendaComponent implements OnInit, OnDestroy {
     const cita = this.citas.find((citaAux: ICita) => citaAux.id === +info.event.id);
     if (cita != null) {
       cita.fechaHoraCita = moment(info.event.start);
-      this.citaService.update(cita).subscribe(p => {});
+      this.citaService.update(cita).subscribe(() => {});
     }
   }
 
@@ -104,6 +115,7 @@ export class AgendaComponent implements OnInit, OnDestroy {
     const element = this.events.find(citaAux => citaAux.id === +e.event.id);
     const modalRef = this.modalService.open(CitaDeleteDialogComponent, { size: 'lg', backdrop: 'static' });
     modalRef.componentInstance.cita = element;
+    this.citaSel = element;
   }
 
   loadTratamientos(elements: ICita[]): void {
@@ -134,7 +146,11 @@ export class AgendaComponent implements OnInit, OnDestroy {
     this.changeMode();
   }
 
-  ngOnDestroy(): void {}
+  ngOnDestroy(): void {
+    if (this.eventSubscriber) {
+      this.eventManager.destroy(this.eventSubscriber);
+    }
+  }
 
   changeMode(): void {
     this.options = { ...this.options, defaultView: this.modeSel };
@@ -142,5 +158,11 @@ export class AgendaComponent implements OnInit, OnDestroy {
 
   cancel(): void {
     this.modalCita = false;
+  }
+
+  registerChangeInCitaDelete(): void {
+    this.eventSubscriber = this.eventManager.subscribe('citaListModification', () => {
+      this.options.remove(this.citaSel.id);
+    });
   }
 }
