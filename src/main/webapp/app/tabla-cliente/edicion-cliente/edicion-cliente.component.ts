ce3745c11f974c112e1dbba28697c0ef86688b86
x231458 <apozogar@everis.com>
2020-07-18 13:30:30 +0200

    cambios

--- src/main/webapp/app/tabla-cliente/edicion-cliente/edicion-cliente.component.ts
+++ src/main/webapp/app/tabla-cliente/edicion-cliente/edicion-cliente.component.ts
@@ -1,14 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Cliente } from 'app/shared/model/cliente.model';
 
 @Component({
   selector: 'jhi-edicion-cliente',
   templateUrl: './edicion-cliente.component.html'
 })
 export class EdicionClienteComponent implements OnInit {
-
-  constructor() { }
+  public cliente: Cliente = new Cliente();
+  constructor(
+    protected activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.activatedRoute.data.subscribe(({ cliente }) => {
+      if (cliente != null && cliente.id != null) {
+        this.cliente = cliente;
+      }
+    });
   }
+
+
 
 }
